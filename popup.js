d2f9af5da0f7692363017773d7c598f0e33f4920
Eric <epffisher@gmail.com>
2013-12-30 14:16:24 -0600

    Change internal pathname

--- popup.js
+++ popup.js
@@ -20,16 +20,23 @@ iosubmit.addEventListener("click", function(event) {
 	event.preventDefault();
 	console.log("Stopped click");
 	var radio = null;
+	var checkbox = document.getElementById("passwords");
+	var passwords = null;
 	for (var i = 0, length = radios.length; i < length; i++) {
 		if (radios[i].checked) {
 			radio = radios[i].value;
 			break;
 		}
 	}
+	if (checkbox.checked) {
+		passwords = 1;
+	} else {
+		passwords = 0;
+	}
 	xhr.open("POST", "http://10.3.35.30/lco/internal/io.php", true);
 	xhr.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-	xhr.send("tech="+document.getElementById("tech").value+"&user="+document.getElementById("user").value+"&inc="+document.getElementById("inc").value+"&inorout="+radio);
-	console.log("In or out: "+radio);
+	xhr.send("tech="+document.getElementById("tech").value+"&user="+document.getElementById("user").value+"&inc="+document.getElementById("inc").value+"&inorout="+radio+"&passwords="+passwords+"&notes="+document.getElementById("notes").value);
+	console.log("In or out: "+radio+" and passwords: "+passwords);
 	xhr.onreadystatechange = function() {
 		if (xhr.readyState == 4) {
 			document.body.innerHTML = xhr.responseText;
