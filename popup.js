a8e8f3efd47d92a96b2d1251ccb85176516bc02e
Eric <epffisher@gmail.com>
2014-01-13 13:14:12 -0600

    Add notes placeholder/redirect to PHP folder

--- popup.js
+++ popup.js
@@ -33,7 +33,7 @@ iosubmit.addEventListener("click", function(event) {
 	} else {
 		passwords = 0;
 	}
-	xhr.open("POST", "http://10.3.35.30/lco/internal/io.php", true);
+	xhr.open("POST", "http://10.3.35.30/lco/internal/io/io.php", true);
 	xhr.setRequestHeader("Content-type","application/x-www-form-urlencoded");
 	xhr.send("tech="+document.getElementById("tech").value+"&user="+document.getElementById("user").value+"&inc="+document.getElementById("inc").value+"&inorout="+radio+"&passwords="+passwords+"&notes="+document.getElementById("notes").value);
 	console.log("In or out: "+radio+" and passwords: "+passwords);
